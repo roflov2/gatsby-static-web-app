25b66f360832177b21f08284c491dabb646d54cd
roflov2 <43810192+roflov2@users.noreply.github.com>
2020-12-22 15:50:58 +1100

    First commit...yay

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,9 +8,9 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <h1>Hi Nic and Ian</h1>
+    <p>Eelcome to this shitty temp site.</p>
+    <p>We can have CC and Coke here... I think.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
